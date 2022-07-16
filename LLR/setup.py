0af020606d708ccf47a31a5219b4d3de0cf7c99d
Ashod Khederlarian <81383507+ashod1@users.noreply.github.com>
2022-07-16 10:57:12 -0400

    Add files via upload

---
+++ LLR/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="LLR",
+    version="1.0",
+    description="Local linear regression package",
+    url="https://github.com/AlanPearl/pypackbasics",
+    author="Ashod Khederlarian",
+    author_email="ask126@pitt.edu",
+    license="MIT",
+    python_requires=">=3.6"
+    install_requires=[
+        "sklearn",
+        "numpy",
+    ],
+    packages=find_packages()
+)

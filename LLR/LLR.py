52ed6e98d8f8aa865104ab9b331004af9b5b9102
ashodkh <ask126@pitt.edu>
2023-01-12 06:53:08 -0800

    Option to add weights

--- LLR/LLR.py
+++ LLR/LLR.py
@@ -1,7 +1,7 @@
 import numpy as np
 from sklearn.neighbors import KDTree
 
-def LLR(x1, x1_train, y1_train, nn, weight):
+def LLR(x1, x1_train, y1_train, nn, weight, extra_weights = None):
     '''
     Local linear regression with inverse distance weight and nn number of nearest neighbors.
     
@@ -53,19 +53,21 @@ def LLR(x1, x1_train, y1_train, nn, weight):
     W = np.zeros([n_valid,nl,nl])
     X = np.zeros([n_valid,nl,x1.shape[1]])
     Y = np.zeros([n_valid,nl,1])
+    if extra_weights is None:
+        extra_weights = np.ones(nl)
     if weight == 'constant':
         for j in range(nl):
-            W[:,j,j] = 1
+            W[:,j,j] = 1*extra_weights[j]
             X[:,j,:] = x1_train[ind[:,j],:]
             Y[:,j,0] = y1_train[ind[:,j]]
     elif weight == 'inverse_distance':
         for j in range(nl):
-            W[:,j,j] = 1/dist[:,j]
+            W[:,j,j] = 1/dist[:,j]*extra_weights[j]
             X[:,j,:] = x1_train[ind[:,j],:]
             Y[:,j,0] = y1_train[ind[:,j]]
     elif weight == 'inverse_distance_squared':
         for j in range(nl):
-            W[:,j,j] = 1/dist[:,j]**2
+            W[:,j,j] = 1/dist[:,j]**2*extra_weights[j]
             X[:,j,:] = x1_train[ind[:,j],:]
             Y[:,j,0] = y1_train[ind[:,j]]
     else:
@@ -81,7 +83,7 @@ def LLR(x1, x1_train, y1_train, nn, weight):
         
     return y_fit, zeros
 
-def LLR_slow(x1, x1_train, y1_train, nn, weight):
+def LLR_slow(x1, x1_train, y1_train, nn, weight, extra_weights = None):
     '''
     Local linear regression with inverse distance weight and nn number of nearest neighbors.
     
@@ -108,6 +110,10 @@ def LLR_slow(x1, x1_train, y1_train, nn, weight):
     zeros: indices corresponding to points in x1 that have nearest neighbor at zero distance. If the data is good quality, this shouldn't happen.
     
     '''
+    ones = np.ones([x1.shape[0],1])
+    x1 = np.concatenate((ones,x1), axis=1)
+    ones = np.ones([x1_train.shape[0],1])
+    x1_train = np.concatenate((ones,x1_train), axis=1)
     
     nl = nn
     tree = KDTree(x1_train[:,:])
@@ -129,20 +135,22 @@ def LLR_slow(x1, x1_train, y1_train, nn, weight):
     X = np.zeros([nl,x1.shape[1]])
     Y = np.zeros([nl,1])
     y_fit = np.zeros(n_valid)
+    if extra_weights is None:
+        extra_weights = np.ones(nl)
     for i in range(n_valid):
         if weight == 'constant':
             for j in range(nl):
-                W[j,j] = 1
+                W[j,j] = 1*extra_weights[j]
                 X[j,:] = x1_train[ind[i,j],:]
                 Y[j,0] = y1_train[ind[i,j]]
         elif weight == 'inverse_distance':
             for j in range(nl):
-                W[j,j] = 1/dist[i,j]
+                W[j,j] = 1/dist[i,j]*extra_weights[j]
                 X[j,:] = x1_train[ind[i,j],:]
                 Y[j,0] = y1_train[ind[i,j]]
         elif weight == 'inverse_distance_squared':
             for j in range(nl):
-                W[j,j] = 1/dist[i,j]**2
+                W[j,j] = 1/dist[i,j]**2*extra_weights[j]
                 X[j,:] = x1_train[ind[i,j],:]
                 Y[j,0] = y1_train[ind[i,j]]
         else:
